0583faad298affcb8ac6d3066d3a52b94bce343b
khaledmk <khaled.korany2004@gmail.com>
2025-09-28 19:54:48 +0300

    db seperation

--- src/MechanicShop.Contracts/Requests/WorkOrders/LoginRequest.cs
+++ src/MechanicShop.Contracts/Requests/WorkOrders/LoginRequest.cs
@@ -6,11 +6,11 @@ public class LoginRequest
 {
     [Required(ErrorMessage = "Email is required.")]
     [EmailAddress(ErrorMessage = "Invalid email format.")]
-    public string Email { get; set; } = string.Empty;
+    public string Email { get; set; } = "pm@localhost";
 
     [Required(ErrorMessage = "Password is required.")]
     [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
-    public string Password { get; set; } = string.Empty;
+    public string Password { get; set; } = "pm@localhost";
 
     public bool RememberMe { get; set; }
 }
